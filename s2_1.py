7d28c12516392d3e8d9b3bed16723c53f729ab8c
amirrezarobaty84 <87778168+amirrezarobaty84@users.noreply.github.com>
2021-08-10 18:22:58 +0430

    Update s2_1.py

--- s2_1.py
+++ s2_1.py
@@ -1,1 +1,14 @@
+number1 = int(input("enter first number \n"))
+number2 = int(input("enter second number \n"))
+amalgar = input("amalghar khod ra vared konid \n")
+if amalgar == "+":
+    print(number1 + number2)
+elif amalgar == "-":
+    print(number1 - number2)
+elif amalgar == "/":
+    print(number1 / number2)
+elif amalgar == "*":
+    print(number1 * number2)
+else:
+    print("man in amalgar ro nemishenasam")
 
