bb301abfcc1d9a65ee63920fb66a0e56d5fb82bc
amirrezarobaty84 <87778168+amirrezarobaty84@users.noreply.github.com>
2021-08-10 18:50:14 +0430

    Update s2h_1.py

--- s2h_1.py
+++ s2h_1.py
@@ -1,1 +1,17 @@
+#salam ostad in tamrin bayad yek adda begire
+#va bege ke in addad be 2 bakhsh pazir hast ya na.
+#man ba 2 ravash raftam in tamrin ro albate ba ejaze shoma:)
+#first method:
+number = int(input("please enter your number \n"))
+number2 = number % 2
+if number2 == 0 :
+    print("your number is divisible by 2 ")
+else:
+    print("your number is not divisible by 2")
+#second method:
+number = int(input("please enter your number \n"))
+if number %2 == 0 :
+    print("your number is divisible by 2 ")
+else:
+    print("your number is not divisible by 2")
 
