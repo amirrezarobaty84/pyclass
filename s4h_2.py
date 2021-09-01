f640cca208bf4661c7250e4670c97f7493ad1f33
amirrezarobaty84 <87778168+amirrezarobaty84@users.noreply.github.com>
2021-09-01 17:48:25 +0430

    Create s4h_2.py

---
+++ s4h_2.py
@@ -0,0 +1,13 @@
+#DAR IN TAMRIN BAYAD MOSHASAT ZIR RA GEREFTE VA DAR YEK DICTIOANRY ZAKHIRE konim:
+#ESM
+#NAME KHANEVADEGI
+#SEN
+#VAZIAT TAHOL
+
+Name = input("Please enter your name \n")
+Last_Name = input("Please enter your last name \n")
+Age = int(input("Please enter your age \n"))
+Marital_Status= input("Please enter your marital status \n")
+
+Specifications = {'Name':Name, 'Last_Name':Last_Name, 'Age':Age, 'Martial_Status':Marital_Status}
+print(Specifications)
