53055a9ed59c4277cc333776ef385c4c468c70a2
Amirreza Robaty <87778168+amirrezarobaty84@users.noreply.github.com>
2021-09-08 14:51:47 +0430

    Create s5h_3.py

---
+++ s5h_3.py
@@ -0,0 +1,12 @@
+#tamrin sevom
+#bayad esm haye daron list ro khat be khat benevisim
+
+names = ['amirreza', 'ali', 'amir', 'zahra', 'roozbeh']
+starting = 0
+for name,index in enumerate(names):
+    print(name,index)
+    taghir=(names[starting])
+    new = str(taghir)
+    for char in new:
+        print(char)
+    starting += 1
