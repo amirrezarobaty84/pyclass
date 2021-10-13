c61f78ac8abe6c58431a547d02b53de913dd8206
Amirreza Robaty <87778168+amirrezarobaty84@users.noreply.github.com>
2021-10-13 16:45:17 +0330

    Create contacts_edited.py

---
+++ contacts_edited.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+print("HELLO DEAR :) \nDon't have GOOD DAY, Have a GREAT DAY \nI  can do the following: \
+\n1-Add some body if you want\n2-delete some body if you want \n3-Search any body \n4-Show your list \n5-stop")
+
+while True:
+    REQUEST = input("what can i do for you ?!? \n( search - add - delete - show_list - stop ) \n")
+    if REQUEST  == 'add':
+        first_name = input("what's her/his first_name ? \n")
+        last_name = input("what's her/his last_name ? \n")
+        phone_number = int(input("what's her/his phone_number ?\n"))
+        connection = sqlite3.connect('contacts.db')
+        query = "INSERT INTO person VALUES (?,?,?)"
+        cursor = connection.cursor()
+        cursor.execute(query, (first_name, last_name, phone_number))
+        connection.commit()
+        print("{} {} with this phone_number {} added :)".format(first_name, last_name, phone_number))
+
+    elif REQUEST  == 'delete':
+        first_name = input("what's her/his first_name ? \n")
+        last_name = input("what's her/his last_name ? \n")
+        conn = sqlite3.connect('contacts.db')
+        query = "DELETE FROM person WHERE first_name = ? AND last_name = ?"
+        cursor = conn.cursor()
+        cursor.execute(query, (first_name, last_name))
+        conn.commit()
+        print("{} {} deleted :)".format(first_name, last_name, phone_number))
+
+
+    elif REQUEST  == 'search':
+        conn = sqlite3.connect('contacts.db')
+        c = conn.cursor()
+        first_name = input("what's her/his first_name ? \n")
+        last_name = input("what's her/his last_name ? \n")
+        c.execute(f'SELECT * from person where First_name=? and Last_name =? ',  (first_name, last_name))
+        back = c.fetchall()
+        for i in back:
+                print(f'First_name: {i[0]} \t  Last_name: {i[1]} \t phone_number: {i[2]}')
+                conn.commit()
+
+    elif REQUEST  == 'show_list':
+        connection = sqlite3.connect('contacts.db')
+        cursor = connection.cursor()
+        query = "select * from person"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        row = cursor.fetchone()
+        for i in rows:
+            print(f'First_name: {i[0]} \t  Last_name: {i[1]}  \t phone_number: {i[2]} ')
+        connection.commit()
+
+
+    elif REQUEST == 'stop':
+        print("Have a grreat Day :)")
+        break
